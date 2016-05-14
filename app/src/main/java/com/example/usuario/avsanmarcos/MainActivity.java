39222c467e78d8bd95ca79cc53156a8fbb0a7586
deadcool <luiz.kawai@unmsm.edu.pe>
2016-05-14 01:20:32 -0500

    fragment de vista inicial y de universidad vacias

--- app/src/main/java/com/example/usuario/avsanmarcos/MainActivity.java
+++ app/src/main/java/com/example/usuario/avsanmarcos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.usuario.avsanmarcos;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.usuario.fragment.MainFragment;
+import com.example.usuario.fragment.Universidad;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +44,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame,new MainFragment()).commit();
     }
 
     @Override
@@ -77,11 +84,11 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        FragmentManager fragmentManager = getFragmentManager();
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new Universidad()).commit();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
